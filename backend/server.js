55a03b108954754d0cc98861dbc3c68089fa371c
Stefano <stefanocutristecco@gmail.com>
2024-09-14 10:08:28 -0300

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const countriesRoutes = require('./routes/countries');
 const countryInfoRoutes = require('./routes/countryInfo');
 
@@ -7,6 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000', // frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Headers
+}));
 
 // Routes
 app.use('/api/countries', countriesRoutes);
